9a233447cb92c15fbba00d41a4e7ee3354fbdda8
ybuchi <yabuch_ch@hotmail.com>
2022-02-23 11:30:30 -0500

    commit tree a333fe38287aeba0af7c304b4f4d9de598234989

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(flair = "*"){
+    return function sentence(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+};
+
+
+
